df05beead2bc181c6c35a5b27c618f6a37e0d053
Nitesh Jha <nitesh7258@gmail.com>
2022-09-01 21:15:56 +0530

    Day 1

--- Day 1/password_gen.py
+++ Day 1/password_gen.py
@@ -1,14 +1,12 @@
 #1. Create a greeting for your program.
-print("Hello users")
+print("Hi user")
 
 #2. Ask the user for the city that they grew up in.
-city = input("enter ur birth city: \n")
-
+name = input("Enter your city name: \n")
 #3. Ask the user for the name of a pet.
-pet = input("enter ur pet name: \n")
-
+pet = input("Enter your name? \n")
 #4. Combine the name of their city and pet and show them their band name.
-brandname = city + pet
-
+password = name + pet
 #5. Make sure the input cursor shows on a new line, see the example at:
-print(f"{brandname.title()} is our suggested brand name for your new product")
+print(f"Your new password is, {passwrod}")
+#   https://replit.com/@appbrewery/band-name-generator-end
--- Everyday planner.md
+++ Everyday planner.md
@@ -1,15 +1,9 @@
 # 2022-python-challenge
-#started this challenge on 2nd March 2022
+#started this challenge on 30th August 2022
 
 
 ## 8-03-2022 -- #Day 0 of 100 days of coding 
-Built new github and git ssh key
-Created new respository for this challenge and updated everything in it
-
-## 8-03-2022 -- #Day 1 of 100 days of coding -- Day 1, 2 and 3 from Dr. Angela's bootcamp
-Built:
-1. Brand name Genrator
-2. Tip Calculator
-3. Treasure Island game
+- Built new github and git ssh key
+- Created new respository for this -challenge and updated everything in it
 
 
