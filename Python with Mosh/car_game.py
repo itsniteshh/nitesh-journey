1ce1ab28be154b5a608ca8a7abfa73e22b31d1b6
Nitesh <nitesh7258@gmail.com>
2022-11-01 20:29:44 +0530

    Day 2 updated

---
+++ Python with Mosh/car_game.py
@@ -0,0 +1,26 @@
+user_input = ""
+started = False
+stopped = True
+
+while True:
+    #Build a car game that prompts help manual when user types help 
+    user_input = input("> ").lower()
+    
+    if user_input == "help":
+        print("""
+              Start - to start the car \nstop - to stop the car \nquit - to exit the game
+              """.strip())
+        
+    elif user_input == "quit":
+        break
+    
+    elif user_input == "start":
+        started = True
+        print("Car started.... Ready to go")
+    
+    elif user_input == "stop":
+        stopped = False
+        print("Car stopped")
+        
+    else:
+        print("Sorry, I don't understand your language!")
