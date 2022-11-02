2f60e4cdc4ecb328b385fe9be429c931b47dccdc
Nitesh <nitesh7258@gmail.com>
2022-11-02 19:59:42 +0530

    Day 3

---
+++ Python with Mosh/smallest_num.py
@@ -0,0 +1,11 @@
+#to find the smallest num in the list
+
+nums = [3, 6, 2, 8, 1, 4, 10, 5, 80, ]
+
+smallest_num = 0
+
+for num in nums:
+    if num < smallest_num:
+        smallest_num = num
+        
+print(f"The smallest number is {smallest_num}")
