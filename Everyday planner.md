c3a84a5a6df6cf3a198980008f8ffbfe86168e8b
Nitesh Jha <nitesh7258@gmail.com>
2022-11-27 19:23:50 +0530

    Day 1

--- Everyday planner.md
+++ Everyday planner.md
@@ -2,4 +2,5 @@
 #started this challenge again and for the final time on 26TH Nov, 2022
 
 
-#
+###Day 1 -- started with Python with Mosh course first, also halfway through SQL with Mosh
+
---
+++ Python with Mosh/smallest_num.py
@@ -0,0 +1,13 @@
+
+numbers = [5, 19, 3, 0, 42, 13, 84, -1, 94]
+
+#str to store the smallest num which alrady has first num from list
+smallest_num = numbers[0]
+
+
+for num in numbers:
+    if num < smallest_num:
+        smallest_num = num
+        
+print(f"The smallest number is {smallest_num}")
+        
