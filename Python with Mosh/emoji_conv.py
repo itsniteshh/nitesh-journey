17e39b534f6d2ac46deaf2f70e441281719a9ad8
Nitesh Jha <nitesh7258@gmail.com>
2022-11-30 16:47:36 +0530

    Day 4

--- Python with Mosh/emoji_conv.py
+++ Python with Mosh/emoji_conv.py
@@ -6,6 +6,7 @@ output = ""
 user_input = input("> ").split()
 
 for emo in user_input:
-    if emo in emojis.keys():
-        output += emojis.get(emo)
-        print(output)
+    output += emojis.get(emo, emo) + " "
+        
+        
+print(output)
