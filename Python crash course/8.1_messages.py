5f4ca5ebc2d6f88741854a86bb7d715df52c7821
Nitesh Jha <nitesh7258@gmail.com>
2023-01-01 19:12:50 +0530

    Day 1

---
+++ Python crash course/8.1_messages.py
@@ -0,0 +1,8 @@
+#a func called display msgs that prints about our learning
+
+def display_msg():
+    message = "I am learning more about functions"
+    print(message)
+
+display_msg()
+
---
+++ Python crash course/8.2_fav_book.py
@@ -0,0 +1,6 @@
+def fav_book(title):
+    #a func which return fav book
+    book = f"My favourite book is {title}"
+    print(book)
+    
+fav_book("The courage to be disliked")
