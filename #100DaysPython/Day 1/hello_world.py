3437272ea3f3d3f885901dd378423053dcf100fe
Nitesh Jha <nitesh7258@gmail.com>
2023-01-13 16:37:50 +0530

    Day 10

--- #100DaysPython/Day 1/hello_world.py
+++ #100DaysPython/Day 1/hello_world.py
@@ -1,20 +1,47 @@
 #print("HEllo world")
 
-def how_much_i_love_you(nb_petals):
-    # using dict to store answers  
-    answers = {
-       1: "I love you",
-       2:  "a little",
-       3:  "a lot",
-       4:  "passionately",
-       5:  "madly",
-       6: "not at all"
-    }
-    if nb_petals >= 7:
-        nb_petals = nb_petals % 6
-        
-    
-    return answers.get(nb_petals)
 
-x = how_much_i_love_you(12)
-print(x)
+
+#from art import logo
+
+def calculator_program(number1, number2, operation):
+  #function to work the calc program
+  if operation == "+":
+    total = num1 + num2
+  elif operation == "-":
+    total = num1 - num2
+  elif operation == "*":
+    total = num1 * num2
+  elif operation == "/":
+    total = num1 / num2
+  else:
+    print("Wrong input. Try again")
+  return total
+
+
+while True:
+  #print(logo)
+  #loop to continuously ask user for inputs
+  num1 = int(input("what's the first number?: "))
+  print("\n+\n-\n*\n/")
+  op = input("Pick an operation: ")
+  num2 = int(input("Enter the second number?: "))
+  
+  total = calculator_program(num1, num2, op)
+
+  print(f"{num1} {op} {num2} = {total}")
+
+  
+  another_try = input(f"Type 'y' to continue calculating using {total}, or type 'n' to start new calculation:  ").lower()
+
+  if another_try == "quit":
+    break
+  elif another_try == "y":
+    num1 = total
+  elif another_try == 'n':
+    pass
+
+
+
+
+
