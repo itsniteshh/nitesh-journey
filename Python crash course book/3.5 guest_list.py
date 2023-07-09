f11890f7e430067d115fc0f4fe7201695bdd838d
Nitesh Jha <nitesh7258@gmail.com>
2023-07-09 15:01:13 +0530

    Day 9

---
+++ Python crash course book/3.5 guest_list.py
@@ -0,0 +1,4 @@
+guests = ["Elon musk", "Nicholas Tesla", "Albert Einstien", "da vinci", "Galileo", "Alaxendar the great", "Tutankhamen"]
+
+for n in guests:
+    print(f"You are invited to the dinner {n}")
