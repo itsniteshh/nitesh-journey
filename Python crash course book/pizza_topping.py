b8989545aab97eec55062494129628e38a9b0f6e
Nitesh Jha <nitesh7258@gmail.com>
2023-07-11 17:15:22 +0530

    Day 11

---
+++ Python crash course book/pizza_topping.py
@@ -0,0 +1,8 @@
+
+while True:
+    want = input("which pizza topping would you like to have? ")
+    
+    if want == "quit":
+        break
+    else:
+        print(f"We are adding {want} to your pizza")
